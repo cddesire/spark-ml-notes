56186ed3b86dbd577ad08bfd286756a7c60820da
changdong.cd <changdong.cd@alibaba-inc.com>
2016-12-23 09:33:05 +0800

    numClasses

--- src/main/scala/org/apache/spark/mllib/tree/configuration/Strategy.scala
+++ src/main/scala/org/apache/spark/mllib/tree/configuration/Strategy.scala
@@ -68,12 +68,12 @@ import org.apache.spark.mllib.tree.configuration.QuantileStrategy._
  */
 @Since("1.0.0")
 class Strategy @Since("1.3.0") (
-    @Since("1.0.0") @BeanProperty var algo: Algo,  //选择的算法，有分类和回归两种选择
-    @Since("1.0.0") @BeanProperty var impurity: Impurity,  //纯度有熵、基尼系数、方差三种选择
-    @Since("1.0.0") @BeanProperty var maxDepth: Int,    //树的最大深度
-    @Since("1.2.0") @BeanProperty var numClasses: Int = 2,  //分类数
-    @Since("1.0.0") @BeanProperty var maxBins: Int = 32,   //最大子树个数
-    @Since("1.0.0") @BeanProperty var quantileCalculationStrategy: QuantileStrategy = Sort, //保存类别变量以及相应的离散值。一个entry (n ->k) 表示特征n属于k个类别，分别是0,1,...,k-1
+    @Since("1.0.0") @BeanProperty var algo: Algo,  // 选择的算法，有分类和回归两种选择
+    @Since("1.0.0") @BeanProperty var impurity: Impurity,  // 纯度有熵、基尼系数、方差三种选择
+    @Since("1.0.0") @BeanProperty var maxDepth: Int,    // 树的最大深度
+    @Since("1.2.0") @BeanProperty var numClasses: Int = 2,  // 分类数
+    @Since("1.0.0") @BeanProperty var maxBins: Int = 32,   // 最大子树个数
+    @Since("1.0.0") @BeanProperty var quantileCalculationStrategy: QuantileStrategy = Sort, // 保存类别变量以及相应的离散值。一个entry (n ->k) 表示特征n属于k个类别，分别是0,1,...,k-1
     @Since("1.0.0") @BeanProperty var categoricalFeaturesInfo: Map[Int, Int] = Map[Int, Int](),
     @Since("1.2.0") @BeanProperty var minInstancesPerNode: Int = 1,
     @Since("1.2.0") @BeanProperty var minInfoGain: Double = 0.0,

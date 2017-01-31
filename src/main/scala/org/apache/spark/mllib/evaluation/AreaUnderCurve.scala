4b562fcdc985fbafbb46de24748f033aba7beb1c
changdong.cd <changdong.cd@alibaba-inc.com>
2017-01-31 11:13:00 +0800

    trapezoidal

--- src/main/scala/org/apache/spark/mllib/evaluation/AreaUnderCurve.scala
+++ src/main/scala/org/apache/spark/mllib/evaluation/AreaUnderCurve.scala
@@ -25,6 +25,16 @@ import org.apache.spark.mllib.rdd.RDDFunctions._
  */
 private[evaluation] object AreaUnderCurve {
 
+  /**
+   * Uses the trapezoidal rule to compute the area under the line connecting the two input points.
+   * @param points two 2D points stored in Seq
+   */
+  private def trapezoid(points: Seq[(Double, Double)]): Double = {
+    require(points.length == 2)
+    val x = points.head
+    val y = points.last
+    (y._1 - x._1) * (y._2 + x._2) / 2.0
+  }
 
   /**
    * Returns the area under the given curve.

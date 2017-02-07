397b9495f620527b4f6eec970ec43c0125495be1
changdong.cd <changdong.cd@alibaba-inc.com>
2017-02-07 09:09:41 +0800

    term frequency

--- src/main/scala/org/apache/spark/mllib/feature/HashingTF.scala
+++ src/main/scala/org/apache/spark/mllib/feature/HashingTF.scala
@@ -76,4 +76,11 @@ class HashingTF(val numFeatures: Int) extends Serializable {
     dataset.map(this.transform)
   }
 
+  /**
+   * Transforms the input document to Transforms vectors (Java version).
+   */
+  @Since("1.1.0")
+  def transform[D <: JavaIterable[_]](dataset: JavaRDD[D]): JavaRDD[Vector] = {
+    dataset.rdd.map(this.transform).toJavaRDD()
+  }
 }
